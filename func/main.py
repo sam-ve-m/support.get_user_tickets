f3856a0495df9ba5e7513bd9f6003d27a970b8b3
Vinicius Reis <vro@lionx.com.br>
2022-03-03 15:37:48 -0300

    update requests.args and lib versions (nidavelir and heimdall)

--- func/main.py
+++ func/main.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import asyncio
 from flask import request, Response
 
 from src.validator import Filter
@@ -7,20 +8,25 @@ from src.service import ClientTicketListService
 from nidavellir.src.uru import Sindri
 from heimdall_client.bifrost import Heimdall
 
+
 log = logging.getLogger()
+event_loop = asyncio.get_event_loop()
 
 
 def fn():
     url_path = request.full_path
-    raw_account_changes_params = request.json
+    raw_account_changes_params = request.args
     x_thebes_answer = request.headers.get('x-thebes-answer')
     heimdall_client = Heimdall(logger=log)
 
     try:
         http_status = 403
         payload = {"status": False}
-        if heimdall_client.validate_jwt(jwt=x_thebes_answer):
-            jwt_content, heimdall_status = heimdall_client.decode_payload(jwt=x_thebes_answer)
+        is_a_valid_jwt = event_loop.run_until_complete(heimdall_client.validate_jwt(jwt=x_thebes_answer))
+        if is_a_valid_jwt:
+            jwt_content, heimdall_status = event_loop.run_until_complete(
+                heimdall_client.decode_payload(jwt=x_thebes_answer)
+            )
             filter_params = Filter(**raw_account_changes_params)
             client_account_change_service = ClientTicketListService(
                 params=filter_params, url_path=url_path, x_thebes_answer=jwt_content['decoded_jwt']
